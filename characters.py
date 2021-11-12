46da30af5510b4eaa6ae776b7b7d7071569d202e
Richard Santana <richardsantana@protonmail.com>
2021-11-12 02:33:55 -0500

    h

--- characters.py
+++ characters.py
@@ -4,32 +4,25 @@
 
 import random
 
-class Creature:
-    def __init__(self, name, power, health):
+class Player:
+    def __init__(self, name, power, health, exp):
         self.name = name
         self.power = power
         self.health = health
         self.exp = exp
 
     def attack(self):
-        multiplier = random.uniform(5,6)
+        multiplier = 10 * random.random()
         damage = self.power * multiplier
-        print (f"{self.name} deals {damage} damage!\n")
+        print (f"{self.name} deals {damage} damage!")
         return damage
 
-class Player(Creature):
-    def __init__(self, name, power, health, exp):
-        super().__init__(name, power, health)
-        self.exp = exp
+class Enemy:
+    def __init__(self, power, health):
+        self.power = power
+        self.health = health
 
-    def heal():
-        multiplier = random.uniform(3,4)
-        recovery = self.power * muliplier
-
-
-class Enemy(Creature):
-    def __init__(self, name, power, health):
-        super().__init__(name, power, health)
+hhhhh
 
 # class Imp(Enemy):
 #
